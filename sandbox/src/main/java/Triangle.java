a38bcbb8921cbaeaff34be82c5b9b0022247b3a6
Ksenia Bolotskikh <madam.bolotsckix2017@yandex.ru>
2025-03-18 10:13:36 +0300

    В конструктор добавлены выбросы исключений, созданы тесты для проверки

--- sandbox/src/main/java/Triangle.java
+++ sandbox/src/main/java/Triangle.java
@@ -1,20 +1,26 @@
-public class Triangle {
+public class Triangle{
     double a;
     double b;
     double c;
 
-    public Triangle(double a, double b, double c)
-    {
+    public Triangle(double a, double b, double c){
         this.a = a;
         this.b = b;
         this.c = c;
+
+        if(a < 0 || b < 0 || c < 0){
+            throw new IllegalArgumentException("Triangle side should be non-negative");
+        }
+        if(a + b < c || a + c < b || b + c < a){
+            throw new IllegalArgumentException("The triangle inequality is violated");
+        }
     }
 
-    public double Perimeter(){
+    public double perimeter(){
         return this.a + this.b + this.c;
     }
 
-    public double Area(){
-        return((Math.sqrt((Perimeter() / 2.0) * ((Perimeter() / 2.0 ) - this.a) * ((Perimeter() / 2.0 ) - this.b) * ((Perimeter() / 2.0 ) - this.c))));
+    public double area(){
+        return((Math.sqrt((perimeter() / 2.0) * ((perimeter() / 2.0 ) - this.a) * ((perimeter() / 2.0 ) - this.b) * ((perimeter() / 2.0 ) - this.c))));
     }
 }
--- sandbox/src/test/java/TriangleTest.java
+++ sandbox/src/test/java/TriangleTest.java
@@ -4,15 +4,34 @@ import org.junit.jupiter.api.Test;
 public class TriangleTest {
 
 @Test
-    void TestPerimeter () {
+    void testPerimeter () {
         var triangle = new Triangle(3.0, 3.0, 3.0);
-        double result = triangle.Perimeter();
+        double result = triangle.perimeter();
         Assertions.assertEquals(9.0, result);
     }
+
 @Test
-    void TestArea () {
+    void testArea () {
         var triangle = new Triangle(3.0, 3.0, 3.0);
-        double result = triangle.Area();
+        double result = triangle.area();
         Assertions.assertEquals(3.897114317029974, result);
+    }
+
+@Test
+    void negativeSideA(){
+    try {
+       new Triangle(-3.0, 3.0, 3.0);
+       Assertions.fail();
+    } catch (IllegalArgumentException exception) {
+    }
+    }
+
+@Test
+    void triangleInequality(){
+        try {
+            new Triangle(1.0, 5.0, 10.0);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+        }
     }
 }
