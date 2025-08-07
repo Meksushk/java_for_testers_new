db2a28d269c20932a6cece3ea640ee9b5b8f5402
Ksenia Bolotskikh <madam.bolotsckix2017@yandex.ru>
2025-08-07 22:22:59 +0300

    Лекция 10.2

--- addressbook_web_tests/src/test/java/manager/HibernateHelper.java
+++ addressbook_web_tests/src/test/java/manager/HibernateHelper.java
@@ -1,5 +1,6 @@
 package manager;
 
+import io.qameta.allure.Step;
 import manager.hbm.AddressInGroups;
 import manager.hbm.AddressRecord;
 import manager.hbm.GroupRecord;
@@ -63,6 +64,7 @@ public class HibernateHelper extends HelperBase {
         return new AddressRecord(Integer.parseInt(id), data.firstname(), data.lastname(), data.mobile());
     }
 
+    @Step
     public List<GroupData> getGroupList() {
         return convertList(sessionFactory.fromSession(session -> {
             return session.createQuery("from GroupRecord", GroupRecord.class).list();
@@ -103,6 +105,7 @@ public class HibernateHelper extends HelperBase {
         });
     }
 
+    @Step
     public void createGroup(GroupData groupData) {
         sessionFactory.inSession(session -> {
             session.getTransaction().begin();
