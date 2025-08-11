ae7307933a3bd6d62f6afb6baeccf8834c8023ca
Ksenia Bolotskikh <madam.bolotsckix2017@yandex.ru>
2025-08-11 22:25:04 +0300

    Revert "Лекция 9.3"
    This reverts commit 9686228cf350d7da4bc3afd27396f5118bc8b44a.

--- mantis_tests/src/test/java/ru/stqa/mantis/manager/developerMail/GetIdsResponse.java
+++
@@ -1,7 +0,0 @@
-package ru.stqa.mantis.manager.developerMail;
-
-import java.util.List;
-
-public record GetIdsResponse(Boolean success, Object errors, List<String> result) {
-
-}
--- mantis_tests/src/test/java/ru/stqa/mantis/manager/developerMail/GetMessageResponse.java
+++
@@ -1,4 +0,0 @@
-package ru.stqa.mantis.manager.developerMail;
-
-public record GetMessageResponse(Boolean success, Object errors, String result) {
-}
